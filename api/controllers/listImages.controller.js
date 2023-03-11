c92f470611e58bc12947f0026f6f3759221458f4
Fazlul Hoque <37170785+fazlulShanto@users.noreply.github.com>
2023-03-11 21:24:36 +0600

    deleted json

--- api/controllers/listImages.controller.js
+++ api/controllers/listImages.controller.js
@@ -1,16 +1,16 @@
 
 
-// const listOfFiles = [
-//     "https://images2.imgbox.com/5d/be/lbRLHRzA_o.jpg",
-//     "https://images2.imgbox.com/19/8b/ltzYHTVL_o.jpg",
-//     "https://images2.imgbox.com/14/45/VZ02uE9y_o.jpg",
-//     "https://images2.imgbox.com/a8/0b/Bs0WKJs2_o.jpg",
-//     "https://images2.imgbox.com/ab/51/KyQWTFGF_o.jpg"
-// ];
-const {list} = require('./list.json');
+const listOfFiles = [
+    "https://images2.imgbox.com/5d/be/lbRLHRzA_o.jpg",
+    "https://images2.imgbox.com/19/8b/ltzYHTVL_o.jpg",
+    "https://images2.imgbox.com/14/45/VZ02uE9y_o.jpg",
+    "https://images2.imgbox.com/a8/0b/Bs0WKJs2_o.jpg",
+    "https://images2.imgbox.com/ab/51/KyQWTFGF_o.jpg"
+];
+// const {list} = require('./list.json');
 
 const getRandomImage = ()=>{
-    const listOfFiles = list; 
+    // const listOfFiles = list; 
     const guess = Math.floor(Math.random() * listOfFiles.length);
     const randomImage= listOfFiles[guess];
     return randomImage;
